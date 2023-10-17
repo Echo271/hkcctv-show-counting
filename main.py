412e4c49f9b7d60ea3a45edbe685dd0b438b29d7
Echo271 <erdarmawan7@gmail.com>
2023-10-17 16:52:01 +0800

    adding iframe for streaming

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/history')
+def history():
+    return 'Ini History'
+
 if __name__ == "__main__":
     app.run(debug=True)
