a3406b3adda67b6ab383ef869a89672af1f99b42
Echo271 <erdarmawan7@gmail.com>
2023-10-24 13:27:46 +0800

    api of history success

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import requests
 from flask import Flask, render_template
 
+database = 'http://api-hkiot.kazuyosan.my.id:8081/database/history/HKCCTV_DH001/guest_count/get_all'
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +10,9 @@ def index():
 
 @app.route('/history')
 def history():
-    return render_template('history.html')
+    response = requests.get(database)
+    data = response.json()
+    return render_template('history.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
